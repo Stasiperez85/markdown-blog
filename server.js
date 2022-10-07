66c65ab6c722bc06cb7fb0beaff850f2cfeeee73
Stasiperez85 <Stasiperez85@gmail.com>
2022-10-06 20:27:22 -0600

    updated all articles and routes

--- server.js
+++ server.js
@@ -1,30 +1,23 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
 const app = express()
 
-mongoose.connect('mongodb://localhost/blog')
-
+mongoose.connect('mongodb://localhost/blog', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+})
 
 app.set('view engine', 'ejs')
-
-app.use('/articles', articleRouter)
 app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
 
-app.get('/', (req, res) => {
-    const articles = [{
-        title: 'Test Article',
-        createAt: new Date(),
-        description: 'Test description'
-    },
-    {
-        title: 'Test Article 2',
-        createAt: new Date(),
-        description: 'Test description 2'
-    }]
-
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({ createdAt: 'desc' })
     res.render('articles/index', { articles: articles })
 })
 
+app.use('/articles', articleRouter)
 
 app.listen(5000)
